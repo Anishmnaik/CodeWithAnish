c1e2bd537a9b15c709cec848e86152e0f62d2deb
Anishmnaik <anishmanjunathnaik@gmail,com>
2026-02-17 23:48:00 +0530

    adding the notes of assembly, numpy, pandas and cpp program

---
+++ cpp programming/first program/helloworld.cpp
@@ -0,0 +1,13 @@
+#include<iostream> //i- input ,o- output,stream- connection
+using namespace std;
+int main(){
+   std::cout<<"hello world anish";           //used for printing     
+   cout<<"my name is anish";
+   return 0; 
+}
+
+
+//single line comment
+   /* multi line comment*/
+
+   
